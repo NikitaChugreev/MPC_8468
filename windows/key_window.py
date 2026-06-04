159a619df9dd6caf3023a4baecc41e37a76703d8
Nikita Chugreev <139382073+NikitaChugreev@users.noreply.github.com>
2026-06-04 14:34:31 +0300

    upd

--- windows/key_window.py
+++ windows/key_window.py
@@ -70,8 +70,8 @@ class KeyWindow(QtWidgets.QMainWindow, Ui_KeyWindow):
                 self.NumberLine.setText('')
             self.LabelTextLim.setText(self.translator.tr('gas_flow_max'))
             self.LabelTextPar.setText(self.translator.tr('gas_flow') + ' ' + self.label_sender[2])
-            self.max_limit = 30
-            self.min_limit = 0.5
+            self.min_limit = settings.get('MIN_FLOW_RRG', 0.5)
+            self.max_limit = settings.get('MAX_FLOW_RRG', 30)
             self.message = self.translator.tr('error_gas_flow_max')
 
         elif self.label_sender == 'TimeZad':
@@ -114,57 +114,71 @@ class KeyWindow(QtWidgets.QMainWindow, Ui_KeyWindow):
         self.ButtonCheck.setText(self.translator.tr('select'))
 
     def button_point(self, button):
-        if button == 'ButtonPoint' and not str.endswith(self.NumberLine.text(), '.') and len(self.NumberLine.text()) != 0 and '.' not in self.NumberLine.text():
-                self.NumberLine.setText(self.NumberLine.text() + '.')
+        time_str = self.NumberLine.text()
+
+        if button == 'ButtonPoint' and not str.endswith(time_str, '.') and len(time_str) != 0 and '.' not in time_str:
+                self.NumberLine.setText(time_str + '.')
 
         elif button == 'ButtonPoint_1':
-            if len(self.NumberLine.text()) == 0:
-                self.NumberLine.setText(self.NumberLine.text() + '00:')
+            if len(time_str) == 0:
+                self.NumberLine.setText(time_str + '00:')
 
         elif button == 'ButtonPoint_2':
-            if len(self.NumberLine.text()) == 3:
-                self.NumberLine.setText(self.NumberLine.text() + '00')
-            elif len(self.NumberLine.text()) == 2:
-                self.NumberLine.setText(self.NumberLine.text() + ':00')
-            elif len(self.NumberLine.text()) == 1:
-                self.NumberLine.setText('0' + self.NumberLine.text() + ':00')
+            if len(time_str) == 3:
+                self.NumberLine.setText(time_str + '00')
+            elif len(time_str) == 2:
+                self.NumberLine.setText(time_str + ':00')
+            elif len(time_str) == 1:
+                self.NumberLine.setText('0' + time_str + ':00')
     
     def check(self):
         is_valid = True
+        text_str = self.NumberLine.text()
 
         if self.label_sender in ['TimeZad']:
-            if self.NumberLine.text() != '' and self.NumberLine.text()[-1] == ':':
-                getattr(self.parent(), self.label_sender).setText(self.NumberLine.text() + '00')
-            elif len(self.NumberLine.text()) == 1:
-                getattr(self.parent(), self.label_sender).setText('0' + self.NumberLine.text() + ':00')
-            elif len(self.NumberLine.text()) == 2:
-                getattr(self.parent(), self.label_sender).setText(self.NumberLine.text() + ':00')
-            elif len(self.NumberLine.text()) == 4 and self.NumberLine.text().startswith('00:'):
-                getattr(self.parent(), self.label_sender).setText('00:0' + self.NumberLine.text()[3])
+            formatted = text_str
+            if text_str != '' and text_str[-1] == ':':
+                formatted = text_str + '00'
+            elif len(text_str) == 1:
+                formatted = '0' + text_str + ':00'
+            elif len(text_str) == 2:
+                formatted = text_str + ':00'
+            elif len(text_str) == 4 and text_str.startswith('00:'):
+                formatted = '00:0' + text_str[3]
             else:
-                getattr(self.parent(), self.label_sender).setText(self.NumberLine.text())
+                formatted = text_str
+
+            try:
+                minutes, seconds = map(int, formatted.split(':'))
+                if 0 <= minutes <= 59 and 0 <= seconds <= 59:
+                    getattr(self.parent(), self.label_sender).setText(formatted)
+                else:
+                    is_valid = False
+                    self.message = self.translator.tr('enter_valid_value')
+            except:
+                is_valid = False
         
-        elif self.NumberLine.text() != '':
-            if self.min_limit <= float(self.NumberLine.text()) <= self.max_limit:
+        elif text_str != '':
+            if self.min_limit <= float(text_str) <= self.max_limit:
                 if self.label_sender in ['ClockVE0', 'ClockNI', 'PressZad', 'HFPowerZad', 'VE1FlowZad', 'VE2FlowZad', 'VE3FlowZad', 'VE4FlowZad']:
                     if self.label_sender in ['VE1FlowZad', 'VE2FlowZad', 'VE3FlowZad', 'VE4FlowZad']:
-                        getattr(self.parent(), self.label_sender).setText(str(float(self.NumberLine.text())))
+                        getattr(self.parent(), self.label_sender).setText(str(float(text_str)))
                     elif self.label_sender == 'HFPowerZad':
-                        getattr(self.parent(), self.label_sender).setText(str(int(float(self.NumberLine.text()))))
+                        getattr(self.parent(), self.label_sender).setText(str(int(float(text_str))))
                     else:
-                        getattr(self.parent(), self.label_sender).setText(self.NumberLine.text())
+                        getattr(self.parent(), self.label_sender).setText(text_str)
                 elif self.label_sender == 'coef_rrg1':
-                    self.parent().userInputRrgConvCoeffPlace_1.setText(self.NumberLine.text())
+                    self.parent().userInputRrgConvCoeffPlace_1.setText(text_str)
                 elif self.label_sender == 'coef_rrg2':
-                    self.parent().userInputRrgConvCoeffPlace_2.setText(self.NumberLine.text())
+                    self.parent().userInputRrgConvCoeffPlace_2.setText(text_str)
                 elif self.label_sender == 'coef_rrg3':
-                    self.parent().userInputRrgConvCoeffPlace_3.setText(self.NumberLine.text())
+                    self.parent().userInputRrgConvCoeffPlace_3.setText(text_str)
                 elif self.label_sender == 'coef_rrg4':
-                    self.parent().userInputRrgConvCoeffPlace_4.setText(self.NumberLine.text())
+                    self.parent().userInputRrgConvCoeffPlace_4.setText(text_str)
             else:
                 is_valid = False
 
-        if self.NumberLine.text() == '':
+        if text_str == '':
             if self.label_sender in ['VE1FlowZad', 'VE2FlowZad', 'VE3FlowZad', 'VE4FlowZad', 'PressZad']:
                 getattr(self.parent(), self.label_sender).setText('0.0')
             elif self.label_sender == 'coef_rrg1':
